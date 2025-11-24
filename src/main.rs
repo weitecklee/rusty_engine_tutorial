521bb89fecdc6c8772ca0df43b4c9616e3d69b99
arkteck <arkteck@gmail.com>
2025-11-24 10:10:19 -0500

    engine init, game state, game logic func

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use rusty_engine::prelude::*;
+
+#[derive(Resource)]
+struct GameState {
+    current_score: u32,
+    high_score: u32,
+    enemy_labels: Vec<String>,
+    spawn_timer: Timer,
+}
+
+impl Default for GameState {
+    fn default() -> Self {
+        Self {
+            current_score: 0,
+            high_score: 0,
+            enemy_labels: Vec::new(),
+            spawn_timer: Timer::from_seconds(1.0, TimerMode::Once),
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut game = Game::new();
+
+    game.add_logic(game_logic);
+    game.run(GameState::default());
+}
+
+fn game_logic(engine: &mut Engine, game_state: &mut GameState) {
+    game_state.current_score += 1;
+    println!("Current score: {}", game_state.current_score);
 }
